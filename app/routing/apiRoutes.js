4f220989299a50272febebae3dc8773e229ba42f
CB721 <clintbrodar@gmail.com>
2019-06-23 15:42:46 -0700

    Form validation

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,6 +2,7 @@
 var friends = require("../data/friends")
 
 module.exports = function(app) {
+    // json friends
     app.get("/api/friends", function(req, res) {
         res.json(friends);
     })
